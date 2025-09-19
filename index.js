075d4788102b6cb1f950f9d89ceebb1a3d0c59ad
jimyelectron <jnowakowski30@reverestudent.org>
2025-09-18 21:41:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,11 +37,7 @@ const firebaseConfig = {
 };
 
 
-// Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
 };
 
 const app = initializeApp(firebaseConfig);
