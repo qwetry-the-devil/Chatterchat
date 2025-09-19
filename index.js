38684256315a799918d0f748d65627373b124c0b
jimyelectron <jnowakowski30@reverestudent.org>
2025-09-18 21:44:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore, setDoc, doc, getDoc, updateDoc, arrayUnion, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 // Firebase config
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyAU1SHuBd24zNgP11D6aOPV3w0YFxz8bso",
-
   authDomain: "cchhatteerr.firebaseapp.com",
-
   projectId: "cchhatteerr",
-
   storageBucket: "cchhatteerr.firebasestorage.app",
-
   messagingSenderId: "462333840338",
-
   appId: "1:462333840338:web:81b2a196992783a7ea160b",
-
   measurementId: "G-H9M070PFZB"
-
-};
-
-
-
 };
 
 const app = initializeApp(firebaseConfig);
